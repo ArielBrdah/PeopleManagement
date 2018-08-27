a9c2d14d975959da6a20299f22567f0cb1469596
aribo <aribo@10.0.0.5>
2018-08-27 18:12:55 +0300

    People Management Application v1.0

---
+++ PeopleManagement/src/fr/testfx/personnes/model/Sexe.java
@@ -0,0 +1,12 @@
+package fr.testfx.personnes.model;
+
+public enum Sexe {
+	MASCULIN("Masculin"),
+	FEMININ("Féminin"),
+	INCONNU("Inconnu");
+	
+	private String name = "";
+	
+	Sexe(String n){name = n;}
+	public String toString() {return name;}
+}
